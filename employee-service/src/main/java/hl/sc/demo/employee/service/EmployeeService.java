b8914a41a0021be60fea7c51c68809d6dac2487a
huangliang <260015120@qq.com>
2019-09-10 18:08:45 +0800

    指定路径

--- employee-service/src/main/java/hl/sc/demo/employee/service/EmployeeService.java
+++ employee-service/src/main/java/hl/sc/demo/employee/service/EmployeeService.java
@@ -3,6 +3,7 @@ package hl.sc.demo.employee.service;
 import hl.sc.demo.employee.model.Employee;
 import hl.sc.demo.employee.repository.EmployeeMongoRepository;
 import hl.sc.demo.employee.repository.EmployeeRedisRepository;
+import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
@@ -30,14 +31,14 @@ public class EmployeeService {
     @Autowired
     private ReactiveRedisOperations<String, Employee> reactiveRedisOperations;
 
-    @GraphQLMutation
+    @GraphQLMutation(name = "addEmployee")
     public Mono<Employee> add(Employee employee) {
         log.info("Employee add: {}", employee);
         return employeeMongoRepository.save(employee);
     }
 
-    @GraphQLMutation
-    public Mono<BigInteger> delete(BigInteger id) {
+    @GraphQLMutation(name = "delEmployee")
+    public Mono<BigInteger> delete(@GraphQLArgument(name = "deleteId") BigInteger id) {
         log.info("Employee delete: id={}", id);
         String key = Employee.EMPLOYEE + "_" + id;
         return employeeMongoRepository.deleteById(id)
@@ -48,7 +49,7 @@ public class EmployeeService {
 
     }
 
-    @GraphQLQuery
+    @GraphQLQuery(name = "employee")
     public Mono<Employee> findById(BigInteger id) {
         log.info("Employee find: id={}", id);
         String key = Employee.EMPLOYEE + "_" + id;
@@ -64,19 +65,19 @@ public class EmployeeService {
                                                       ));
     }
 
-    @GraphQLQuery
+    @GraphQLQuery(name = "employees")
     public Flux<Employee> findAll() {
         log.info("Employee find");
         return employeeRedisRepository.findAll();
     }
 
-    @GraphQLQuery
+    @GraphQLQuery(name = "employees")
     public Flux<Employee> findByDepartment(Long departmentId) {
         log.info("Employee find: departmentId={}", departmentId);
         return employeeRedisRepository.findByDepartment(departmentId);
     }
 
-    @GraphQLQuery
+    @GraphQLQuery(name = "employees")
     public Flux<Employee> findByOrganization(Long organizationId) {
         log.info("Employee find: organizationId={}", organizationId);
         return employeeRedisRepository.findByOrganization(organizationId);
